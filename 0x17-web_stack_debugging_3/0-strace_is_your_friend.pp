31f9fba03503fe3d99deb27078a20935f50ccdbb
afro <aigbemanuel@gmail.com>
2024-02-20 10:32:15 -0800

    fixed error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,9 +1,15 @@
 # Fixing Apache returning a 500 error
 
 
+file { '/var/www/html/wp-settings.php':
+  ensure  => file,
+  content => file('/var/www/html/wp-settings.php').content.gsub('phpp', 'php'),
+  require => Exec['fixing error'],
+}
+
 exec { 'fixing error':
-	provider => 'shell',
-	command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
-	path => '/bin:/usr/bin',
-	onlyif => 'test -f /var/www/html/wp-settings.php',
+  command  => '/bin/true',
+  path     => '/bin:/usr/bin',
+  onlyif   => 'test -f /var/www/html/wp-settings.php && grep -q "phpp" /var/www/html/wp-settings.php',
+  provider => shell,
 }
