e7621d3841357209853461a470507cc552a2bf0b
afro <aigbemanuel@gmail.com>
2024-02-20 10:50:50 -0800

    fixed err

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,15 +1,7 @@
-# Fixing Apache returning a 500 error
+#  fixes typo ext error to php in the wp-file
 
 
-file { '/var/www/html/wp-settings.php':
-  ensure  => file,
-  content => file('/var/www/html/wp-settings.php').content.gsub('phpp', 'php'),
-  require => Exec['fixing error'],
-}
-
-exec { 'fixing error':
-  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
-  path     => '/bin:/usr/bin',
-  onlyif   => 'test -f /var/www/html/wp-settings.php && grep -q "phpp" /var/www/html/wp-settings.php',
-  provider => 'shell',
+exec { 'fix-wordpress':
+  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+  path    => '/usr/bin:/bin',
 }
